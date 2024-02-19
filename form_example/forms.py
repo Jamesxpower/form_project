37491b540cb4221726c6734339df4c0de7939c04
jamesxpower <jamesxpower@gmail.com>
2024-02-19 16:58:47 +0800

    2024/2/19 study

--- form_example/forms.py
+++ form_example/forms.py
@@ -1,4 +1,6 @@
+from django.core.validators import ValidationError
 from django import forms
+
 
 RADIO_CHOICES = (
     ("Value One", "Value One Display"),
@@ -21,17 +23,56 @@ BOOK_CHOICES = (
     )
 )
 
+def validate_email_domain(value):
+    if value.split("@")[-1].lower() != "example.com":
+        raise ValidationError("The email address must be on the domain example.com.")
+
+
 class ExampleForm(forms.Form):
-    text_input = forms.CharField()
-    password_input = forms.CharField(widget=forms.PasswordInput)
+    text_input = forms.CharField(max_length=3)
+    password_input = forms.CharField(min_length=8, widget=forms.PasswordInput)
     checkbox_on = forms.BooleanField()
     radio_input = forms.ChoiceField(choices=RADIO_CHOICES, widget=forms.RadioSelect)
     favorite_book = forms.ChoiceField(choices=BOOK_CHOICES)
-    books_you_own = forms.MultipleChoiceField(choices=BOOK_CHOICES)
+    books_you_own = forms.MultipleChoiceField(required=False, choices=BOOK_CHOICES)
     text_area = forms.CharField(widget=forms.Textarea)
-    integer_input = forms.IntegerField()
+    integer_input = forms.IntegerField(min_value=1, max_value=10)
     float_input = forms.FloatField()
-    decimal_input = forms.DecimalField(max_digits=3)
+    decimal_input = forms.DecimalField(max_digits=5, decimal_places=3)
     email_input = forms.EmailInput()
     date_input = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
     hidden_input = forms.CharField(widget=forms.HiddenInput, initial="Hidden Value")
+
+class OrderForm(forms.Form):
+    magazine_count = forms.IntegerField(min_value=0, max_value=80,
+                                        widget=forms.NumberInput(attrs={"placeholder": "Number of Magazines"}))
+    book_count = forms.IntegerField(min_value=0, max_value=50,
+                                    widget=forms.NumberInput(attrs={"placeholder": "Number of Books"}))
+    send_confirmation = forms.BooleanField(required=False)
+    email = forms.EmailField(required=False, validators=[validate_email_domain],
+                             widget=forms.EmailInput(attrs={"placeholder": "Your company email address"}))
+
+    def clean_email(self):
+        return self.cleaned_data["email"].lower()
+
+    def clean(self):
+        clean_data = super().clean()
+        if clean_data["send_confirmation"] and not clean_data.get("email"):
+           self.add_error("email", "Please enter an email address to receive the confirmation message.")
+        elif clean_data.get("email") and not clean_data["send_confirmation"]:
+           self.add_error("send_confirmation", "Please check this if you want to receive a confirmation email.")
+
+        item_total = clean_data.get("magazine_count", 0) + clean_data.get("book_count", 0)
+        if item_total > 100:
+            self.add_error(None, "The total number of items must be 100 or less.")
+
+
+class NewsletterSignupForm(forms.Form):
+    signup = forms.BooleanField(label="Sign up to newsletter?", required=False)
+    email = forms.EmailField(help_text="Enter your email address to subscribe", required=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data["signup"] and not cleaned_data.get("email"):
+            self.add_error("email", "Your email address is required if signing up for the newsletter.")
--- form_example/views.py
+++ form_example/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from .forms import ExampleForm
+from .forms import ExampleForm, OrderForm
 
 def form_example(request):
+    initial = {"email": "user@example.com"}
 
     if request.method == "POST":
-        form = ExampleForm(request.POST)
+        #form = ExampleForm(request.POST)
+        form = OrderForm(request.POST, initial=initial)
+
         if form.is_valid():
            for name, value in form.cleaned_data.items():
                print("{}: ({}) {}".format(name, type(value), value))
     else:
-        form = ExampleForm()
+        form = OrderForm(initial=initial)
 
     return render(request=request, template_name="form-example.html",
                   context={"method": request.method, "form": form})
